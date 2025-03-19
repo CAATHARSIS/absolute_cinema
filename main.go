5cb6f1fea312bbe2432afc55cf0e6ecf264b5971
Murlock <vanay2015ooo@yandex.ru>
2025-03-19 06:28:53 +0300

    minor update

--- main.go
+++ main.go
@@ -3,23 +3,41 @@ package main
 import (
 	"net/http"
 
+	"github.com/CAATHARSIS/absolute_cinema/common/pkg/database"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigFile("./pkg/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
+
 	router := gin.Default()
+	database.Init(dbUrl)
 
 	router.LoadHTMLGlob("internal/templates/*")
 
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(
+	// 		http.StatusOK,
+	// 		"index.html",
+	// 		gin.H{
+	// 			"title": "Home Page",
+	// 		},
+	// 	)
+	// })
+
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(
+		c.JSON(
 			http.StatusOK,
-			"index.html",
 			gin.H{
-				"title": "Home Page",
-			},
-		)
+				"port":  port,
+				"dbUrl": dbUrl,
+			})
 	})
 
-	router.Run()
+	router.Run(port)
 }
